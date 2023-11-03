498932b1c27fddb522d8090a71c4a47980568b2c
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2023-11-03 17:18:14 +0100

    Report composants portfolio-1 vers porfolio-2

---
+++ src/components/Home/index.js
@@ -0,0 +1,40 @@
+import './styles.scss';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import LogoS from '../../assets/images/Logo-S.png';
+import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+    return () => clearTimeout(timer);
+  });
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters letterClass={letterClass} strArray={'Hi,'.split('')} idx={12} />
+            <br />
+            <AnimatedLetters letterClass={letterClass} strArray={'I\'m'.split('')} idx={15} />
+            <img src={LogoS} alt="Logo S" />
+            <AnimatedLetters letterClass={letterClass} strArray={'ylvia'.split('')} idx={18} />
+            <br />
+            <AnimatedLetters letterClass={letterClass} strArray={'web developper.'.split('')} idx={23} />
+          </h1>
+          <h2>Full stack developper | PHP | Symfony | React</h2>
+          <Link to="/contact" className="flat-button">CONTACTEZ-MOI</Link>
+        </div>
+        <Logo />
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,72 @@
+import './styles.scss';
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHome, faUser, faEnvelope, faSuitcase,
+} from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import LogoS from '../../assets/images/Logo-S.png';
+import LogoSylvia from '../../assets/images/Logo-Sylvia.png';
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSylvia} alt="Sylvia" />
+      </Link>
+      <nav>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/"
+        >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/sylvia-iannarelli/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Sylvia-iannarelli"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
