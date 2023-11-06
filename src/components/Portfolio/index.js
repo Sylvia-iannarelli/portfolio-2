c2ca6a3324a6f8c9dba2b3c9cb0d5ad68b33d5bf
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2023-11-06 14:19:22 +0100

    Légères retouches avant mise en ligne

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -53,6 +53,7 @@ const Portfolio = () => {
             strArray={'Portfolio'.split('')}
             idx={15}
           />
+          <h2>En cours de construction...</h2>
         </h1>
         <div>{renderPortfolio(portfolioData.portfolio)}</div>
       </div>
